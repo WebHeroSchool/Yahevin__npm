3ade7035168bc9381be11618a6d50915bd9a55c0
Ramazan Abdullin <Yahevin15@gmail.com>
2018-11-19 10:59:50 +0300

    Lesson10_1

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,25 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('buildJs', () => {
 	return gulp.src(['scripts/*.js'])
-		.pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(concat('all.js'))
-        .pipe(uglify())
+		.pipe(sourcemaps.init())
+			.pipe(babel({
+	            presets: ['@babel/env']
+	        }))
+	        .pipe(concat('all.js'))
+	        .pipe(uglify())
+	    .pipe(sourcemaps.write())
 		.pipe(gulp.dest('build/js'));
 	});
 gulp.task('buildCss', () => {
 	return gulp.src(['styles/*.css'])
-		.pipe(concat('all.css'))
-		.pipe(cssnano())
+		.pipe(sourcemaps.init())
+			.pipe(concat('all.css'))
+			.pipe(cssnano())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('build/css'));
 	});
 gulp.task('build', ['buildCss','buildJs']);
