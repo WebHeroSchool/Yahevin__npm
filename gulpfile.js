729cb1c11e653043952c784d8cc454821603fa0f
Ramazan Abdullin <Yahevin15@gmail.com>
2018-11-19 10:07:21 +0300

    Lesson7_1

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
+
 gulp.task('buildJs', () => {
 	return gulp.src(['*.js'])
+		.pipe(babel({
+            presets: ['@babel/env']
+        }))
 		.pipe(gulp.dest('build/js'));
 	});
 gulp.task('buildCss', () => {
